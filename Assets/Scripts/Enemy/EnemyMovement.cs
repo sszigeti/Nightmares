b027fb7ec70542eeff0799c2a51a994060aad2e8
Sandor Szigeti <rollsroyce4sale@gmail.com>
2014-11-12 17:27:56 +0100

    Zombunny follows Player, original code had a bug which is fixed here

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -8,18 +8,18 @@ public class EnemyMovement : MonoBehaviour
     //EnemyHealth enemyHealth;
     NavMeshAgent nav;
 
-
     void Awake ()
     {
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        //player = GameObject.FindGameObjectWithTag ("Player").transform;// For some reason with this the Transform will not get updated, it will be stuck at 0,0,0
+		player = GameObject.Find ("Player").transform;// This is needed for the Transform to be dynamic as expected
         //playerHealth = player.GetComponent <PlayerHealth> ();
         //enemyHealth = GetComponent <EnemyHealth> ();
         nav = GetComponent <NavMeshAgent> ();
     }
 
-
     void Update ()
     {
+		Debug.Log (player.position);
         //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
         //{
             nav.SetDestination (player.position);
